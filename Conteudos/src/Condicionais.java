ce5c78922fe9155d91b8e97cee3ebefe0c9740ff
Fernando <fokamura10@gmail.com>
2024-05-28 15:00:57 -0300

    adicionado source

---
+++ Conteudos/src/Condicionais.java
@@ -0,0 +1,21 @@
+public class Condicionais {
+
+    public static void main(String[] args) {
+/*
+if(se)
+else if(se não se)
+else(se não)
+ */
+        int number = 5;
+
+        if (number < 5){
+            System.out.println("O número " + number + " é menor que 5");
+        }else if(number ==5){
+            System.out.println("O número " + number + " é igual a 5");
+        }else{
+            System.out.println("O número " + number + " é maior que 5");
+        }
+
+    }
+}
+
