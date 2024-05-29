b7c5256c62868ec9b3f246137a8d8cdab8fb5cd9
Fernando <fokamura10@gmail.com>
2024-05-29 14:59:43 -0300

    Exercicio 3 da unidade 5 feito

---
+++ Conteudos/src/Conteudos/Unidade5/Exercicio5n3.java
@@ -0,0 +1,15 @@
+package Conteudos.Unidade5;
+
+public class Exercicio5n3 {
+
+    public static void main(String[] args) {
+
+        int valor;
+
+        for (valor = 0; valor <= 100000;) {
+            System.out.println(valor);
+            valor = valor + 1000;
+
+        }
+    }
+}
