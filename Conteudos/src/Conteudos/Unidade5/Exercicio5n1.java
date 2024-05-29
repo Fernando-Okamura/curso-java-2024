9d5b6fad46d1aa687178289f2f75b89f991300b6
Fernando <fokamura10@gmail.com>
2024-05-29 00:02:03 -0300

    Resolvido exercícios 1 e 2 da unidade 5

---
+++ Conteudos/src/Conteudos/Unidade5/Exercicio5n1.java
@@ -0,0 +1,15 @@
+package Conteudos.Unidade5;
+
+public class Exercicio5n1 {
+
+    public static void main(String[] args) {
+        int contador = 0;
+
+        for (int i = 1; contador < 5; i++) {
+            if (i % 3 == 0) {
+                System.out.println("O número " + i + " é múltiplo de 3.");
+                contador +=1;
+            }
+        }
+    }
+}
---
+++ Conteudos/src/Conteudos/Unidade5/Exercicio5n2.java
@@ -0,0 +1,30 @@
+package Conteudos.Unidade5;
+
+public class Exercicio5n2 {
+
+    public static void main(String[] args) {
+        //resolução 1
+        int i = 1;
+        for (i = 0; i <= 100; i++) {
+            System.out.println(i);
+        }
+        System.out.println("----------------------");
+        //resolução 2
+        i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i = i + 1;
+        }
+        System.out.println("--------------------------");
+
+        i = 1;
+        do{
+            System.out.println( i);
+            i = i + 1;
+        }
+        while (i <=100);
+        }
+
+
+    }
+
