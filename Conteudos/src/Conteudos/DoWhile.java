9b9e82f28af24e5851523cd5564ba30f0c49a9c6
Fernando <fokamura10@gmail.com>
2024-05-28 23:03:06 -0300

    While e do while

---
+++ Conteudos/src/Conteudos/DoWhile.java
@@ -0,0 +1,41 @@
+package Conteudos;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+        //aprendendo while e do while
+        //Recebe dados do usuário via teclado
+        int idade = 0;
+        String nome;
+
+
+        Scanner sc = new Scanner(System.in);
+        //do while primeiro executa o bloco, depois faz a checagem
+        //while checa primeiro e só roda se for verdadeiro, para o loop funcionar
+        // tem que ajeitar para que seja verdadeiro
+        do {
+            System.out.print("Informe o seu nome: ");
+            nome = sc.nextLine();
+            //next sozinho funcion, mas só pega um dado da pessoa, já o nextLine pega sobrenomes
+
+            System.out.print("Informe a sua idade: ");
+            //idade = sc.nextInt(); BUG aparece as duas infos
+
+            idade = Integer.parseInt(sc.nextLine());
+            //aqui o parse recebe uma string do usuário e converte em número inteiro, caso não de pra converter,
+            // ocorre erro de compilação(teria que fazer tratamento do erro)
+
+            System.out.println(nome + " tem " + idade + " anos. ");
+
+        }
+        while (idade > 0);
+
+
+        sc.close();
+    }
+}
+
+
+
---
+++ Conteudos/src/Conteudos/While.java
@@ -0,0 +1,35 @@
+package Conteudos;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+
+        //aprendendo while e do while
+        //Recebe dados do usuário via teclado
+        int idade = 1;
+        String nome;
+
+
+        Scanner sc = new Scanner(System.in);
+        //while só entra nele se for verdadeiro(checa o valor  antes de executar o bloco
+        while (idade > 0) {
+            System.out.print("Informe o seu nome: ");
+            nome = sc.nextLine();
+            //next sozinho funcion, mas só pega um dado da pessoa, já o nextLine pega sobrenomes
+
+            System.out.print("Informe a sua idade: ");
+            //idade = sc.nextInt(); BUG aparece as duas infos
+
+            idade = Integer.parseInt(sc.nextLine());
+            //aqui o parse recebe uma string do usuário e converte em número inteiro, caso não de pra converter,
+            // ocorre erro de compilação(teria que fazer tratamento do erro)
+
+            System.out.println(nome + " tem " + idade + " anos. ");
+
+        }
+
+
+        sc.close();
+    }
+}
