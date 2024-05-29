b9625c3cf4fa51259a342f8befa3618544752ee1
Fernando <fokamura10@gmail.com>
2024-05-29 20:41:22 -0300

    Tipos numéricos

---
+++ Conteudos/src/Conteudos/Unidade6/Numericos.java
@@ -0,0 +1,28 @@
+package Conteudos.Unidade6;
+
+public class Numericos {
+    public static void main(String[] args) {
+
+        //tipos inteiros e reais
+        //tipos primários:
+         int num = 4;
+         short num2 = 4;
+         byte num3 = 4;
+         char num8 = 34;
+
+         //tipos não primários/ primitivas
+        Long num7 = (long) 999999999;
+        Integer num4 = 98;
+        Short num5 = 7;
+        Byte num6 =9;
+        Character num9 = 35;
+
+        System.out.println("int/Integer " + Integer.SIZE +" bits");
+
+        System.out.println("Valor Min int/Integer " + Integer.MIN_VALUE);
+        System.out.println("Valor Max int/Integer " + Integer.MAX_VALUE);
+    }
+
+
+
+}
