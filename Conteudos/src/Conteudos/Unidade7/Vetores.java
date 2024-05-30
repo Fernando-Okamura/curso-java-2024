dcd378edc2d43cb497602120463213726220d38c
Fernando <fokamura10@gmail.com>
2024-05-30 15:10:54 -0300

    Vetores

---
+++ Conteudos/src/Conteudos/Unidade7/Vetores.java
@@ -0,0 +1,23 @@
+package Conteudos.Unidade7;
+
+public class Vetores {
+
+    public static void main(String[] args) {
+
+        //Arrays(vetores uni-dimensionais)
+        //declarando o vetor e especificando o tamanho do vetor
+        int numbers[] = new int[5];
+
+        //declarar e inicializar
+
+        int otherNumbers[] = {1, 3, 5, 7, 9};
+
+        int i;
+        String cores[] = {"pink", "purple", "red"};
+
+        for (i= 0; i <= 2; i++){
+            System.out.println(cores[i]);
+
+        }
+    }
+}
