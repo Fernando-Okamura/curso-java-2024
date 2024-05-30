50a330435728a5cb256be7252e1933cc638d2155
Fernando <fokamura10@gmail.com>
2024-05-29 21:51:56 -0300

    Alfanuméricos, Booleanos e Operações Matemáticas

---
+++ Conteudos/src/Conteudos/Unidade6/Alfanumericos.java
@@ -0,0 +1,14 @@
+package Conteudos.Unidade6;
+
+public class Alfanumericos {
+
+    public static void main(String[] args) {
+        // Tipos primitivos
+        char letra1 = 'a';
+        char letra2= 97; // em número decimal
+
+        //tipos nao primitivos
+        Character letra3 = 'A';
+        String nome = "Fernando";
+    }
+}
---
+++ Conteudos/src/Conteudos/Unidade6/Booleanos.java
@@ -0,0 +1,27 @@
+package Conteudos.Unidade6;
+
+public class Booleanos {
+    public static void main(String[] args) {
+
+        //tipos primitivos
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        //tipos nao primitivos
+        Boolean v = true;
+        Boolean f = false;
+
+        System.out.println("Verdadeiro " + verdadeiro);
+        System.out.println("Falso " + falso);
+        System.out.println("V " + v);
+        System.out.println("F " + f);
+
+        if (verdadeiro){
+            System.out.println("É verdadeiro");
+        }else{
+            System.out.println("É falso");
+        }
+
+
+    }
+}
